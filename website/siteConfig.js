d53c643c894a87752dd928d3910061fbf7768202
David Boyne <davidboyne123@hotmail.co.uk>
2018-09-22 22:17:54 +0100

    Updated to docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: "garie", // Title for your website.
   tagline: "An open source toolkit to monitor web performance",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
+  url: "https://garie.io", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,7 +31,8 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: "garie",
-  organizationName: "facebook",
+  organizationName: "boyney123",
+  editUrl: "https://github.com/facebook/docusaurus/edit/master/docs/",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -60,7 +61,7 @@ const siteConfig = {
   scrollToTop: true,
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} David Boyne`,
+  copyright: `Made by David Boyne`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
@@ -82,11 +83,11 @@ const siteConfig = {
 
   // Open Graph and Twitter card images.
   ogImage: "img/docusaurus.png",
-  twitterImage: "img/docusaurus.png"
+  twitterImage: "img/docusaurus.png",
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  repoUrl: "https://github.com/boyney123/garie"
 };
 
 module.exports = siteConfig;
